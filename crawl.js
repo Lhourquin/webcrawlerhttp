e08c7db211ababcc16fdaa668e13bf16294fae1b
lhourquin <lhourquin@gmail.com>
2024-06-26 03:46:17 +0200

    func: normalizeURL

--- crawl.js
+++ crawl.js
@@ -1,5 +1,10 @@
 function normalizeURL(stringURL){
-	return stringURL;	
+	const urlObj = new URL(stringURL);
+	const hostPath =`${urlObj.hostname}${urlObj.pathname}`; 
+	if(hostPath.length > 0 && hostPath.slice(-1) == '/'){
+		return hostPath.slice(0, -1);
+	}
+	return hostPath;
 }
 
 module.exports = {
