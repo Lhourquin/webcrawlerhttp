1185f285f22422121b0bc6a0b96abbf51817cc38
lhourquin <lhourquin@gmail.com>
2024-07-02 05:52:58 +0200

    test: get text from element in html

--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,20 @@
-const {normalizeURL} = require('./crawl.js');
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js');
 const {test, expect} = require('@jest/globals');
+
+
+test("getURLsFromHTML get text from element", ()=>{
+	const HTMLbodyInput = `<html>
+		<body>
+			<p>Hello Lucas, no to hard to use qwerty ?</p>
+		</body>
+	</html>`;
+	const baseURLInput = "http://localhost:8900";
+	const expected ="Hello Lucas, no to hard to use qwerty ?"; 
+	const actual = getURLsFromHTML(HTMLbodyInput, baseURLInput);
+	console.log({expected});
+	console.log({actual});
+	expect(actual).toEqual(expected);
+})
 
 test('normalizeURL strip protocol', ()=> {
 	const input = 'https://blog.boot.dev/path';
@@ -27,4 +42,8 @@ test('normalizeURL strip http', ()=> {
 	console.log({actual});
 	expect(actual).toEqual(expected);
 	
+});
+test('getURLsFromHTML', () => {
+	const inputBaseURL = '';
+	const inputHtmlBody = '';
 });
